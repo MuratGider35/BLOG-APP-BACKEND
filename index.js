242c9295f7562316432163cfb615bdc514fd814b
MuratGider35 <m.gider35@gmail.com>
2024-01-10 00:29:35 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || "127.0.0.1";
+
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "welcome to blog app server",
+  });
+});
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
